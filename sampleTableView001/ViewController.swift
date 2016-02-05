e296127cef5f750e6630a4962484a4205f2c7e28
ei17ringo <ei17ringo@gmail.com>
2016-02-05 23:59:58 +0800

    commit init

--- sampleTableView001/ViewController.swift
+++ sampleTableView001/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // 行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
-
+    // 表示するセルの中身
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+        cell.textLabel!.text = "\(indexPath.row)行目"
+        return cell
+        
+    }
+    
+    // 選択された時に行う処理
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("\(indexPath.row)行目を選択")
+    }
+    
+    // ステータスバーを非表示にする
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 }
 
